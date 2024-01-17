8073f178f49fafcf53792730d47aa2473abe0f89
SangWon7242 <gotkdqja@gmail.com>
2024-01-17 22:58:41 +0900

    원본 코드 테스트 코드에 부여

--- src/app/page.js
+++ src/app/page.js
@@ -1,42 +1,152 @@
 "use client";
 
+import { ThemeProvider } from "@mui/material/styles";
 import * as React from "react";
 import {
+  CssBaseline,
   Button,
-  Toolbar,
   AppBar,
-  Snackbar,
-  Alert as MuiAlert,
-  Backdrop,
-  CircularProgress,
-  Drawer,
-  List,
-  ListItemButton,
-  Tabs,
-  Tab,
+  Toolbar,
+  TextField,
+  Chip,
+  Box,
 } from "@mui/material";
-import { ThemeProvider } from "@mui/material/styles";
+import { FaBars } from "react-icons/fa";
 import theme from "./theme";
-import Link from "next/link";
+import dateToStr from "./dateUtil";
 
-export default function App() {
-  const [tab1CurrentIndex, setTab1CurrentIndex] = React.useState(0);
+function useTodosState() {
+  const [todos, setTodos] = React.useState([]);
+  const lastTodoIdRef = React.useRef(0);
+
+  const addTodo = (newContent) => {
+    const id = ++lastTodoIdRef.current;
+
+    const newTodo = {
+      id,
+      content: newContent,
+      regDate: dateToStr(new Date()),
+    };
+
+    setTodos((todos) => [newTodo, ...todos]);
+  };
+
+  const modifyTodo = (index, newContent) => {
+    const newTodos = todos.map((todo, _index) =>
+      _index != index ? todo : { ...todo, content: newContent }
+    );
+    setTodos(newTodos);
+  };
+
+  const removeTodo = (index) => {
+    const newTodos = todos.filter((_, _index) => _index != index);
+    setTodos(newTodos);
+  };
+
+  return {
+    todos,
+    addTodo,
+    modifyTodo,
+    removeTodo,
+  };
+}
+
+function App() {
+  const todosState = useTodosState(); // 리액트 커스텀훅
+
+  React.useEffect(() => {
+    todosState.addTodo("테니스\n유산소\n축구\n헬스");
+    todosState.addTodo("야구");
+    todosState.addTodo("배구");
+  }, []);
+
+  const onSubmit = (e) => {
+    e.preventDefault();
+
+    const form = e.currentTarget;
+
+    form.content.value = form.content.value.trim();
+
+    if (form.content.value.length == 0) {
+      alert("할 일을 입력해주세요.");
+      form.content.focus();
+      return;
+    }
+
+    todosState.addTodo(form.content.value);
+    form.content.value = "";
+    form.content.focus();
+  };
 
   return (
     <>
-      <ThemeProvider theme={theme}>
-        <Tabs
-          value={tab1CurrentIndex}
-          onChange={(_, newValue) => setTab1CurrentIndex(newValue)}
-        >
-          <Tab label="Item One" />
-          <Tab label="Item Two" />
-          <Tab label="Item Three" />
-        </Tabs>
-        {tab1CurrentIndex == 0 && <div>내용1</div>}
-        {tab1CurrentIndex == 1 && <div>내용2</div>}
-        {tab1CurrentIndex == 2 && <div>내용3</div>}
-      </ThemeProvider>
+      <AppBar position="fixed">
+        <Toolbar>
+          <div className="tw-flex-1">
+            <FaBars className="tw-cursor-pointer" />
+          </div>
+          <div className="logo-box">
+            <a className="tw-font-bold" href="/">
+              HAPPY NOTE
+            </a>
+          </div>
+          <div className="tw-flex-1 tw-flex tw-justify-end">
+            <a href="/write">글작성</a>
+          </div>
+        </Toolbar>
+      </AppBar>
+      <Toolbar />
+      <form
+        onSubmit={(e) => onSubmit(e)}
+        className="tw-flex tw-flex-col tw-p-4 tw-gap-2"
+      >
+        <TextField
+          minRows={3}
+          maxRows={10}
+          multiline
+          name="content"
+          autoComplete="off"
+          label="할 일을 입력해주세요."
+        />
+        <Button variant="contained" className="tw-font-bold" type="submit">
+          추가
+        </Button>
+      </form>
+      <nav className="tw-mt-3 tw-px-4">
+        <ul>
+          {todosState.todos.map((todo) => (
+            <li key={todo.id}>
+              <div className="tw-flex tw-flex-col tw-gap-2 tw-mt-3">
+                <div className="tw-flex tw-gap-x-2 tw-font-bold">
+                  <Chip
+                    className="tw-pt-[3px]"
+                    label={`번호 : ${todo.id}`}
+                    variant="outlined"
+                  />
+                  <Chip
+                    className="tw-pt-[3px]"
+                    label={`현재날짜 : ${todo.regDate}`}
+                    variant="outlined"
+                    color="primary"
+                  />
+                </div>
+                <div className="tw-p-10 tw-rounded-[20px] tw-shadow tw-whitespace-pre-wrap tw-leading-relaxed tw-break-words">
+                  <Box sx={{ color: "primary.main" }}>{todo.content}</Box>
+                </div>
+              </div>
+            </li>
+          ))}
+        </ul>
+      </nav>
     </>
+  );
+}
+
+export default function themeApp() {
+  return (
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <App />
+    </ThemeProvider>
   );
 }
